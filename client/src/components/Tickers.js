6e48a61ee39fe62c5ffebfb76e818e4a7e2dd445
bautiliendo <juanbautistaliendo1@gmail.com>
2024-04-16 16:06:40 -0300

    16/4

--- client/src/components/Tickers.js
+++ client/src/components/Tickers.js
@@ -12,13 +12,13 @@ export const Tickers = () => {
 
     tickersData.forEach(async (ticker) => {
       try {
-        const response = await fetch(`https://api.invertironline.com/api/v2/${ticker.mercado}/Titulos/${ticker.simbolo}/Cotizacion?mercado=${ticker.mercado}&simbolo=${ticker.simbolo}&model.simbolo=${ticker.simbolo}&model.mercado=${ticker.mercado}&model.plazo=${ticker.plazo}`);
-        if (response.ok) {
-          const data = await response.json();
-          console.log(data); // Haz algo con los datos de la respuesta, como guardarlos en el estado o mostrarlos en la interfaz
-        } else {
-          console.error('Error en la petición:', response.statusText);
-        }
+        // const response = await fetch(`https://api.invertironline.com/api/v2/${ticker.mercado}/Titulos/${ticker.simbolo}/Cotizacion?mercado=${ticker.mercado}&simbolo=${ticker.simbolo}&model.simbolo=${ticker.simbolo}&model.mercado=${ticker.mercado}&model.plazo=${ticker.plazo}`);
+        // if (response.ok) {
+        //   const data = await response.json();
+        //   console.log(data); // Haz algo con los datos de la respuesta, como guardarlos en el estado o mostrarlos en la interfaz
+        // } else {
+        //   console.error('Error en la petición:', response.statusText);
+        // }
       } catch (error) {
         console.error('Error en la petición:', error);
       }
@@ -26,15 +26,28 @@ export const Tickers = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []); 
 
-  const añadirTicker = e => {
-    alert(e.target.simbolo.value)
+  const anadirTicker = e => {
+      e.preventDefault()
+      const mercado = e.target.mercado.value;
+      const simbolo = e.target.simbolo.value;
+      const plazo = e.target.plazo.value;
+
+      const newTicker = { mercado, simbolo, plazo };
+      setTickersData([...tickersData, newTicker]);
+  }
+
+  const eliminarTicker = (e, index) => {
+    e.preventDefault();
+    const newTickersData = tickersData.filter((ticker, i) => i !== index);
+    setTickersData(newTickersData);
+    
   }
 
   return (
     <div className='container2'>
         <h1 className='white'> </h1>
         <h2 className='white'>Agregar ticker</h2>
-        <form className='form-tickers' onSubmit={añadirTicker}>
+        <form className='form-tickers' onSubmit={anadirTicker}>
         <select name='mercado' className='input-small'>
             <option value="BCBA">BCBA</option>
             <option value="NYSE">NYSE</option>
@@ -52,13 +65,13 @@ export const Tickers = () => {
         </form>
         <h3 className='white'>Tickers: </h3>
           <div className='tickers-container'>
-          {tickers.map((ticker, index) => (
+          {tickersData.map((ticker, index) => (
             <ul key={index} className='tickers'>
               <li>
                 MERCADO: <strong>{ticker.mercado} </strong>  
                 Símbolo: <strong className='blue'>{ticker.simbolo} </strong>
                 Plazo: <strong>{ticker.plazo[0]}; {ticker.plazo[1]} </strong>
-                <button className='button3'>Eliminar</button>
+                <button className='button3' data-index={index} onClick={(e) => eliminarTicker(e, index)}>Eliminar</button>
               </li>
             
             </ul>
