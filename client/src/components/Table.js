69d389017162130364b48ce1eeb388fbab4591e7
bautiliendo <juanbautistaliendo1@gmail.com>
2024-05-16 12:41:44 -0300

    - Cambiar precio caución manualmente

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -8,6 +8,9 @@ export const Table = () => {
   const [bValuesP, setBValuesP] = useState([]);
   const [bValuesD, setBValuesD] = useState([]);
   const [allSortedData, setAllSortedData] = useState([]);
+  const [caucionPesosManual, setCaucionPesosManual] = useState(caucionPesos);
+  const [caucionDolaresManual, setCaucionDolaresManual] =
+    useState(caucionDolares);
 
   const getCurrentDay = () => {
     const today = new Date();
@@ -31,7 +34,7 @@ export const Table = () => {
     );
     console.log("Dia de la semana: " + getCurrentDay());
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [sortedT0Data, sortedT2Data, caucionPesosManual, caucionDolaresManual]);
 
   useEffect(() => {
     calculateA();
@@ -41,31 +44,38 @@ export const Table = () => {
 
   //Logica para encontrar A ( t2/t0 - 1 )  x 100
 
-// Lógica para encontrar A (puntaCompradoraT2 / puntaVendedoraT0 - 1) x 100
-const calculateA = () => {
-  const newAValues = sortedT0Data.map((t0Ticker, index) => {
-    const t2Ticker = sortedT2Data[index];
-    if (
-      t0Ticker.dataT0 &&
-      t2Ticker.dataT2 &&
-      t0Ticker.dataT0.puntas.length > 0 &&
-      t2Ticker.dataT2.puntas.length > 0
-    ) {
-      // Obtener la primera punta vendedora en T0
-      const puntaVendedoraT0 = t0Ticker.dataT0.puntas[0].precioVenta;
+  // Lógica para encontrar A (puntaCompradoraT2 / puntaVendedoraT0 - 1) x 100
+  const calculateA = () => {
+    const newAValues = sortedT0Data
+      .map((t0Ticker, index) => {
+        const t2Ticker = sortedT2Data[index];
+        if (
+          t0Ticker.dataT0 &&
+          t2Ticker.dataT2 &&
+          t0Ticker.dataT0.puntas.length > 0 &&
+          t2Ticker.dataT2.puntas.length > 0
+        ) {
+          // Obtener la primera punta vendedora en T0
+          const puntaVendedoraT0 = t0Ticker.dataT0.puntas[0].precioVenta;
 
-      // Obtener la primera punta compradora en T2
-      const puntaCompradoraT2 = t2Ticker.dataT2.puntas[0].precioCompra;
+          // Obtener la primera punta compradora en T2
+          const puntaCompradoraT2 = t2Ticker.dataT2.puntas[0].precioCompra;
 
-      // Calcular A
-      const a = ((puntaCompradoraT2 / puntaVendedoraT0) - 1) * 100;
-      return { simbolo: t0Ticker.simbolo, a , puntaVendedoraT0, puntaCompradoraT2};
-    }
-    return null;
-  }).filter(Boolean);
+          // Calcular A
+          const a = (puntaCompradoraT2 / puntaVendedoraT0 - 1) * 100;
+          return {
+            simbolo: t0Ticker.simbolo,
+            a,
+            puntaVendedoraT0,
+            puntaCompradoraT2,
+          };
+        }
+        return null;
+      })
+      .filter(Boolean);
 
-  setAValues(newAValues);
-};
+    setAValues(newAValues);
+  };
 
   // Función para calcular el valor de B según el día de la semana
   const calculateBValue = (caucion) => {
@@ -78,7 +88,7 @@ const calculateA = () => {
       case 1: // Lunes
       case 2: // Martes
       case 3: // Miércoles
-      return (caucion / 365) * 2;
+        return (caucion / 365) * 2;
       case 4: // Jueves
         return (caucion / 365) * 4;
       case 5: // Viernes
@@ -90,8 +100,8 @@ const calculateA = () => {
 
   // Función para calcular los valores de B en pesos y dólares llamando la anterior funcion calculateBValue
   const calculateBvalues = () => {
-    const bValuePesos = calculateBValue(caucionPesos);
-    const bValueDolares = calculateBValue(caucionDolares);
+    const bValuePesos = calculateBValue(caucionPesosManual);
+    const bValueDolares = calculateBValue(caucionDolaresManual);
     setBValuesP(bValuePesos);
     setBValuesD(bValueDolares);
   };
@@ -100,95 +110,128 @@ const calculateA = () => {
     console.log("Array con t0 + t2 ordenado por símbolo:", allSortedData);
   }, [allSortedData]);
 
+  const cambiarCaucionP = (e) => {
+    e.preventDefault();
+    const newValue = parseFloat(e.target.value);
+    if (!isNaN(newValue)) {
+      setCaucionPesosManual(newValue);
+      calculateBvalues();
+    }
+  };
 
-//RETURN ORDENDADO POR OPORTUNIDAD DE TRADE
-return (
-  <div className="body-table">
-    <div className="table-container">
-      <h2 className="h2">Tabla de valores</h2>
-      <div className="info-cauciones">
-        <h4 className="cauciones">Precio caucion en pesos: {caucionPesos}</h4>
-        <ul>
-          <li>
-            B (caucion en pesos / 365)
-            <strong> Lunes - miércoles x 2 =</strong>
-            {(caucionPesos / 365) * 2} ;
-            <strong> Jueves x4 = </strong>
-            {(caucionPesos / 365) * 4} ;
-            <strong> Viernes x4 = </strong>
-            {(caucionPesos / 365) * 4}
-          </li>
-        </ul>
-        <h4>Precio caucion en dólares: {caucionDolares}</h4>
-        <ul>
-        <li>
-            B (caucion en Dolares / 365)
-            <strong> Lunes - miércoles x 2 =</strong>
-            {(caucionDolares / 365) * 2} ;
-            <strong> Jueves x4 = </strong>
-            {(caucionDolares / 365) * 4} ;
-            <strong> Viernes x4 = </strong>
-            {(caucionDolares / 365) * 4}
-          </li>
-        </ul>
+  const cambiarCaucionD = (e) => {
+    e.preventDefault();
+    const newValue = parseFloat(e.target.value);
+    if (!isNaN(newValue)) {
+      setCaucionDolaresManual(newValue);
+      calculateBvalues();
+    }
+  };
+
+  //RETURN ORDENDADO POR OPORTUNIDAD DE TRADE
+  return (
+    <div className="body-table">
+      <div className="table-container">
+        <h2 className="h2">Tabla de valores</h2>
+        <div className="info-cauciones">
+          <h4 className="cauciones">
+            Precio caucion en pesos: {caucionPesosManual}
+          </h4>
+          <ul>
+            <li>
+              B (caución en pesos / 365)
+              <strong> Lunes - miércoles x 2 =</strong>
+              {(caucionPesosManual / 365) * 2};<strong> Jueves x4 = </strong>
+              {(caucionPesosManual / 365) * 4};<strong> Viernes x4 = </strong>
+              {(caucionPesosManual / 365) * 4}
+            </li>
+          </ul>
+          <h4>Precio caución en dólares: {caucionDolaresManual}</h4>
+          <ul>
+            <li>
+              B (caución en Dólares / 365)
+              <strong> Lunes - miércoles x 2 =</strong>
+              {(caucionDolaresManual / 365) * 2};<strong> Jueves x4 = </strong>
+              {(caucionDolaresManual / 365) * 4};<strong> Viernes x4 = </strong>
+              {(caucionDolaresManual / 365) * 4}
+            </li>
+          </ul>
+          <form>
+            <label for="fname">Cambiar valor caución en pesos</label>
+            <input
+              type="text"
+              id="fname"
+              name="fname"
+              onChange={cambiarCaucionP}
+            />
+            <label for="lname">Cambiar valor caución en dolares</label>
+            <input
+              type="text"
+              id="lname"
+              name="lname"
+              onChange={cambiarCaucionD}
+            />
+          </form>
+        </div>
+        {t0Data && t0Data.length > 0 ? (
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Símbolo</th>
+                <th>Mercado</th>
+                <th>Moneda</th>
+                <th>UltimoPrecio T0</th>
+                <th>Punta Vendedora T0</th>
+                <th>Punta Compradora T2</th>
+                <th>A (T2/T0 - 1) x 100</th>
+                <th>B (caucion TNA/ 365)x2 (JU x 4)-(VI X 4)</th>
+                <th>Comparacion A - B</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sortedT0Data
+                .map((ticker, index) => {
+                  const aValue = aValues.find(
+                    (value) => value.simbolo === ticker.simbolo
+                  );
+                  const bValue =
+                    ticker.dataT0.moneda === "peso_Argentino"
+                      ? bValuesP
+                      : bValuesD;
+                  const comparison =
+                    aValue &&
+                    typeof aValue.a === "number" &&
+                    typeof bValue === "number"
+                      ? aValue.a - bValue
+                      : null;
+                  return { ticker, aValue, bValue, comparison };
+                })
+                .filter((item) => item.comparison !== null)
+                .sort((a, b) => b.comparison - a.comparison)
+                .map(({ ticker, aValue, bValue, comparison }, index) => (
+                  <tr
+                    key={index}
+                    style={{
+                      backgroundColor: comparison > 0 ? "#d4edda" : "inherit",
+                    }}
+                  >
+                    <td>{ticker.simbolo}</td>
+                    <td>{ticker.mercado}</td>
+                    <td>{ticker.dataT0.moneda}</td>
+                    <td>{ticker.dataT0.ultimoPrecio}</td>
+                    <td>{aValue ? aValue.puntaVendedoraT0 : "-"}</td>
+                    <td>{aValue ? aValue.puntaCompradoraT2 : "-"}</td>
+                    <td>{aValue ? aValue.a : "-"}</td>
+                    <td>{bValue}</td>
+                    <td>{comparison}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>No hay datos para la tabla</p>
+        )}
       </div>
-      {t0Data && t0Data.length > 0 ? (
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Símbolo</th>
-              <th>Mercado</th>
-              <th>Moneda</th>
-              <th>UltimoPrecio T0</th>
-              <th>Punta Vendedora T0</th>
-              <th>Punta Compradora T2</th>
-              <th>A (T2/T0 - 1) x 100</th>
-              <th>B (caucion TNA/ 365)x2 (JU x 4)-(VI X 4)</th>
-              <th>Comparacion A - B</th>
-            </tr>
-          </thead>
-          <tbody>
-            {sortedT0Data
-              .map((ticker, index) => {
-                const aValue = aValues.find(
-                  (value) => value.simbolo === ticker.simbolo
-                );
-                const bValue =
-                  ticker.dataT0.moneda === "peso_Argentino" ? bValuesP : bValuesD;
-                const comparison =
-                  aValue &&
-                  typeof aValue.a === "number" &&
-                  typeof bValue === "number"
-                    ? aValue.a - bValue
-                    : null;
-                return { ticker, aValue, bValue, comparison };
-              })
-              .filter((item) => item.comparison !== null)
-              .sort((a, b) => b.comparison - a.comparison)
-              .map(({ ticker, aValue, bValue, comparison }, index) => (
-                <tr
-                  key={index}
-                  style={{
-                    backgroundColor: comparison > 0 ? "#d4edda" : "inherit",
-                  }}
-                >
-                  <td>{ticker.simbolo}</td>
-                  <td>{ticker.mercado}</td>
-                  <td>{ticker.dataT0.moneda}</td>
-                  <td>{ticker.dataT0.ultimoPrecio}</td>
-                  <td>{aValue ? aValue.puntaVendedoraT0 : "-"}</td>
-                  <td>{aValue ? aValue.puntaCompradoraT2 : "-"}</td>
-                  <td>{aValue ? aValue.a : "-"}</td>
-                  <td>{bValue}</td>
-                  <td>{comparison}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      ) : (
-        <p>No hay datos para la tabla</p>
-      )}
     </div>
-  </div>
-);
-}
+  );
+};
